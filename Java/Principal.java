93d735112dbf6464a0fcd65cb55fec2518ba02b7
Rondinele Sousa Guimaraes <rondineleg@edu.univali.br>
2019-11-02 03:00:06 -0300

    Atualiando info

--- Java/Principal.java
+++ Java/Principal.java
@@ -3,7 +3,7 @@ package OwllPaper;
 
 public class Principal {
     public static void main(String [] args){
-    Aluno aluno = new Aluno();
+    var aluno = new Aluno();
     
     aluno.nome = "Jorge Campos";
     aluno.matricula = "00221133";
@@ -14,8 +14,8 @@ public class Principal {
     
     System.out.println("Nome: " + aluno.nome + " Matrícula: "
      + aluno.matricula+ " Endereço: " +
-           aluno.endereco+ " Telefone: " + aluno.telefone+ "
-            E-mail: " + aluno.email);
+           aluno.endereco+ " Telefone: " + aluno.telefone + 
+           "E-mail: " + aluno.email);
     
     
     }
