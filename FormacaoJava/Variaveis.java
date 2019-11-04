d6001984495eed38729da14974057e9791a68858
Rondinele <rondineleg@edu.univali.br>
2019-11-04 13:52:00 -0300

    Criando Variaveis com java

---
+++ FormacaoJava/Variaveis.java
@@ -0,0 +1,13 @@
+
+package FormacaoJava;
+
+public class Variaveis 
+{
+    public static void main(String [] args)
+    {
+        int idade;
+        idade = 10;
+
+        System.out.println(idade);
+    }    
+}
