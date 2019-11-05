03fc0f0a7789b61973c9256f883c2c54e7ea82de
Rondinele <rondineleg@edu.univali.br>
2019-11-05 08:55:09 -0300

    Criando alguns dados

--- FormacaoJava/Variaveis.java
+++ FormacaoJava/Variaveis.java
@@ -5,10 +5,17 @@ public class Variaveis
 {
     public static void main(String [] args)
     {
-        int idade;
-        idade = 10;
-        int idadeNoAnoQueVem = idade + 1;
-        
+        // imprime a idade
+        int idade = 20;
+        System.out.println(idade);
+
+        // gera uma idade no ano seguinte
+        int idadeNoAnoQueVem;
+        idadeNoAnoQueVem = idade + 1;
+
+        // imprime a idade
+        System.out.println(idadeNoAnoQueVem);
+
        int quatro = 2 + 2; //Adicao
        int tres = 5 - 2; //Subtracao
        int oito = 4 * 2; //Multiplicacao
@@ -20,6 +27,40 @@ public class Variaveis
        if (idade < 18) 
        {
         System.out.println("Não pode entrar");
+
+        /*
+         comentário daqui,
+        ate aqui
+         */
+
+       // uma linha de comentário sobre a idade
+
+       double pi  = 3.14;
+       double x = 5 * 10;
+       boolean verdade = true;
+       idade = 30;
+
+       boolean menorDeIdade = idade <18;
+
+       char letra = 'A';
+       System.out.println(letra);
+
+       int i = 5; // i recebe uma cópia do valor 5
+       int j = i; // j recebe uma cópia do valor de i
+       i = i + 1; // i vira 6, j continua 5
+
+       //casting
+
+      double d3 = 3.14;
+       int ia = (int) d3;
+
+       long xl = 10000;
+       // int il = xl;  não compila, pois pode estar perdendo informação
+
+
+
+
+
 }      }    
 
 }
