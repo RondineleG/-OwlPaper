e467e49ca063f9e3c4fcb9b9c9364ac845ac63b4
Rondinele <rondineleg@edu.univali.br>
2019-11-05 09:31:37 -0300

    Usando variavel

--- FormacaoJava/Variaveis.java
+++ FormacaoJava/Variaveis.java
@@ -57,7 +57,7 @@ public class Variaveis
        int i = 5; // i recebe uma cópia do valor 5
        int j = i; // j recebe uma cópia do valor de i
        i = i + 1; // i vira 6, j continua 5
-
+       System.out.println(i + j);
        //casting
 
       double d3 = 3.14;
