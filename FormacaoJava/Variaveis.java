58c71a3f848ab3abd2718e3995475bda8abfbb35
Rondinele <rondineleg@edu.univali.br>
2019-11-05 08:44:07 -0300

    Conflitos

--- FormacaoJava/Variaveis.java
+++ FormacaoJava/Variaveis.java
@@ -1,12 +1,26 @@
 
 package FormacaoJava;
 
-public class Principal {
+public class Variaveis 
+{
     public static void main(String [] args)
     {
-    
         int idade;
-        String nome;
-    
-    }    
+        idade = 10;
+        int idadeNoAnoQueVem = idade + 1;
+        
+       int quatro = 2 + 2; //Adicao
+       int tres = 5 - 2; //Subtracao
+       int oito = 4 * 2; //Multiplicacao
+       int dezesseis = 64 / 4; // Divisao
+       int um = 5 % 2; // 5 dividido por 2 dá 2 e tem resto 1, o operador % pega o resto da divisão inteira
+       
+       System.out.println(idadeNoAnoQueVem);
+
+       if (idade < 18) 
+       {
+        System.out.println("Não pode entrar");
+}      }    
+
 }
+
