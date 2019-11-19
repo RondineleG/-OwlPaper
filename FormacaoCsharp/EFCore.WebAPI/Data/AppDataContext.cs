a3e666ec0fcc2add9590a14819408f4d8003cc51
Rondinele <rondineleg@edu.univali.br>
2019-11-19 08:51:37 -0300

    Criando classe AppDataContext

---
+++ FormacaoCsharp/EFCore.WebAPI/Data/AppDataContext.cs
@@ -0,0 +1,22 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.WebAPI.Data
+{
+    public class AppDataContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+       
+        public DbSet<Arma> Armas { get; set; }
+        
+        public DbSet<Batalha> Batalhas { get; set; }
+    
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=den1.mssql8.gear.host;Database=pdadb;User Id=pdadb; Password=Gw6Q_c?VZ9zF");
+        }
+    
+
+    }
+
+}
